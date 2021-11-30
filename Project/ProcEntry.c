03e139fda183bd73821196ae5d301c5fb1226e1c
BoiseState Student <student@u.boisestate.edu>
2021-11-30 15:33:46 -0700

    in progress

--- Project/ProcEntry.c
+++ Project/ProcEntry.c
@@ -9,6 +9,8 @@
 #include <unistd.h>
 #include "ProcEntry.h"
 
+#define MAX_LENGTH 4096
+
 ProcEntry * CreateProcEntry(void) {
     //Allocate space for ProcEntry in heap and validate
     ProcEntry * newProcess = (ProcEntry *) malloc(sizeof(ProcEntry));
@@ -24,24 +26,47 @@ The command field (comm) and path field
 will be allocated on the heap using malloc.
 */
 ProcEntry * CreateProcEntryFromFile(const char statFile[]) {
+    //Create Variables
+    char * dummy = NULL; //holder
+    int process_id; //1
+    char *comm = NULL; //2
+    char state; //3
+    int ppid; //4
+    unsigned long int utime; //14
+    unsigned long int stime; //15
+    long number_threads; //20
+
     //Validate statFile
     if(statFile == NULL) {
         return NULL;
     }
+
     FILE * fp = fopen(statFile, "r");
-    char * dummy;
+
     if(fp == NULL) {
+        fprintf(stderr, "Stat file couldn't open.\n");
         return NULL;  
     } else {
-        fscanf(fp, "%d %s %c %d %lu %lu %ld", &dummy, &dummy, 
-        &dummy, &dummy, &dummy, &dummy, &dummy);
+        fscanf(fp, "%d %s %c %d %s %s %s %s %s %s %s %s %s %lu %lu %s %s %s %s %ld", 
+        &process_id, comm, &state, &ppid, dummy, dummy, dummy, dummy, dummy, dummy, dummy, 
+        dummy, dummy, &utime, &stime, dummy, dummy, dummy, dummy, &number_threads);
     }
 
     //Allocate space for ProcEntry in heap and validate
-    ProcEntry * newProcess = (ProcEntry *) malloc(sizeof(ProcEntry));
+    ProcEntry * newProcess = CreateProcEntry();
     if(newProcess == NULL) {
+        fprintf(stderr, "Couldn't file new ProcEntry.\n");
         return NULL;
     }
+    
+    newProcess->process_id = process_id;
+    newProcess->comm = (char*) malloc(strlen(comm) + 1);        //Allocating space for string
+    strncpy(newProcess->comm, comm, MAX_LENGTH);
+    newProcess->state = state;
+    newProcess->parent_process_id = ppid;
+    newProcess->utime = utime;
+    newProcess->stime = stime;
+    newProcess->number_of_threads = number_threads;
 
     return newProcess;
 }
@@ -52,6 +77,7 @@ void DestroyProcEntry(ProcEntry * entry) {
         return;
     }
 
+    free(entry->comm);
     free(entry);
 }
 
